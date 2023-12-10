758709733b33060877b9b611f29f3d2a0878a5cf
Rahul <rahulsm.27@gmail.com>
2023-12-10 11:05:41 +0530

    Model versioning

--- src/config_schemas/config_schema.py
+++ src/config_schemas/config_schema.py
@@ -1,20 +1,24 @@
 from hydra.core.config_store import ConfigStore
 from pydantic.dataclasses import dataclass
 from omegaconf import MISSING
-
+from typing import Optional
 from src.config_schemas.infrastructure import infrastructure_schema
 from src.config_schemas import base_schemas
 from src.config_schemas.training import training_task_schemas
-
+from src.config_schemas.evaluation import model_selector_schemas
 @dataclass
 class Config:
     infrastructure: infrastructure_schema.InfrastructureConfig = infrastructure_schema.InfrastructureConfig
     save_last_checkpoint_every_n_train_steps : int = 500
     seed: int = 1234
     tasks : dict[str,base_schemas.TaskConfig] = MISSING
+    model_selector : Optional[model_selector_schemas.ModelSelectorConfig] = None
+    registered_model_name:Optional[str] = None
 
 def setup_config() -> None:
     infrastructure_schema.setup_config()
     training_task_schemas.setup_config()
+    model_selector_schemas.setup_config()
+
     cs = ConfigStore.instance()
     cs.store(name="config_schema", node=Config)
--- src/training/lightning_modules/bases.py
+++ src/training/lightning_modules/bases.py
@@ -14,6 +14,7 @@ from src.utils.io_utils import open_file
 
 import os
 import torch
+import mlflow
 
 PartialOptimizerType = Callable[[Union[Iterable[Tensor], dict[str, Iterable[Tensor]]]], Optimizer]
 
@@ -34,8 +35,20 @@ class TrainingLightningModule(LightningModule):
         self.loss = loss
         self.partial_optimizer = optimizer
         self.scheduler = scheduler
-
+        self.model_size = self._calculate_model_size()
         self.logging_logger = get_logger(self.__class__.__name__)
+
+    def _calculate_model_size(self) -> float:
+        param_size = 0
+        for param in self.parameters():
+            param_size += param.nelement() & param.element_size()
+
+        buffer_size = 0
+        for buffer in self.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+
+        size_all_mb = (param_size + buffer_size) / 1024**2
+        return size_all_mb
 
     def configure_optimizers(self)-> Union[Optimizer, tuple[list[Optimizer],list[dict[str,Any]]]] :
         optimizer = self.partial_optimizer(self.parameters())
@@ -46,7 +59,13 @@ class TrainingLightningModule(LightningModule):
         else:
             return optimizer
 
-
+    def on_train_end(self) -> None:
+        try:
+            mlflow.log_metric("model_size", self.model_size)
+        except Exception:
+            pass
+        return super().on_train_end()
+    
     @abstractmethod
     def training_step(self,batch: Any, batch_idx: int)-> Tensor:
         ...
